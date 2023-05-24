56ff5cee63686c176ff168cdc6c885f8e551edc0
NoamR <noam.raby@up42.com>
2023-05-24 11:02:02 +0200

    print filename, handle errors * skips_bad_file passes

--- 03_catr/src/lib.rs
+++ 03_catr/src/lib.rs
@@ -1,19 +1,25 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
-
-pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
-    println!("Hello, world!");
-    Ok(())
-}
 
 #[derive(Debug)]
 pub struct Config {
     files: Vec<String>,
     number_lines: bool,
     number_nonblank_lines: bool,
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(_) => println!("Opened {}", filename),
+        }
+    }
+    Ok(())
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -52,4 +58,11 @@ pub fn get_args() -> MyResult<Config> {
         number_lines: matches.is_present("number_lines"),
         number_nonblank_lines: matches.is_present("number_nonblank_lines"),
     })
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
