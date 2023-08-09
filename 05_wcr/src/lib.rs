9e87694da6872c86fe932b9180cc7fb2a6338ab1
NoamR <noam.raby@up42.com>
2023-08-09 11:51:00 +0200

    parse command arguments

--- 05_wcr/src/lib.rs
+++ 05_wcr/src/lib.rs
@@ -27,14 +27,65 @@ pub fn get_args() -> MyResult<Config> {
                 .multiple(true)
                 .default_value("-"),
         )
+        .arg(
+            Arg::with_name("lines")
+                .value_name("LINES")
+                .long("lines")
+                .short("l")
+                .help("Prints number of lines")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("words")
+                .value_name("WORDS")
+                .long("words")
+                .short("w")
+                .help("Prints number of words")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .value_name("BYTES")
+                .long("bytes")
+                .short("c")
+                .help("Prints number of bytes. Cancels prior -m option")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("chars")
+                .value_name("CHARS")
+                .long("chars")
+                .short("m")
+                .help("Prints number of bytes. Cancels prior -c option")
+                .takes_value(false)
+                .conflicts_with("bytes"),
+        )
         .get_matches();
 
+    let files = matches.values_of_lossy("files").unwrap();
+    let lines = matches.is_present("lines");
+    let words = matches.is_present("words");
+    let bytes = matches.is_present("bytes");
+    let chars = matches.is_present("chars");
+
+    // if found any flag, use flags
+    if lines || words || bytes || chars {
+        return Ok(Config {
+            files,
+            lines,
+            words,
+            bytes,
+            chars,
+        });
+    }
+
+    // defaults to showing lines, words, and bytes when none of the flags was supplied
     Ok(Config {
-        files: vec!["".to_string()],
+        files,
         lines: true,
         words: true,
         bytes: true,
-        chars: true,
+        chars: false,
     })
 }
 
