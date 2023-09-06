e468fe403e2515c79597692a1e8b300359539dc9
NoamR <noam.raby@up42.com>
2023-09-06 11:00:18 +0200

    parse arguments

--- 06_uniqr/src/lib.rs
+++ 06_uniqr/src/lib.rs
@@ -11,10 +11,36 @@ pub struct Config {
 }
 
 pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("uniqr")
+        .version("0.1.0")
+        .author("Noam")
+        .about("Rust uniq command")
+        .arg(
+            Arg::with_name("input_file")
+                .value_name("INPUT_FILE")
+                .help("The input file is the first positional argument and defaults to dash (-)")
+                .required(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("output_file")
+                .value_name("INPUT_FILE")
+                .help("The output file is the second positional argument and is optional"),
+        )
+        .arg(
+            Arg::with_name("count")
+                .value_name("COUNT")
+                .short("c")
+                .long("count")
+                .help("The -c | --count flag is optional")
+                .takes_value(false),
+        )
+        .get_matches();
+
     Ok(Config {
-        in_file: "".to_string(),
-        out_file: Some("".to_string()),
-        count: false,
+        in_file: matches.value_of("input_file").unwrap().to_string(),
+        out_file: Some(matches.value_of("output_file").unwrap().to_string()),
+        count: matches.is_present("count"),
     })
 }
 
