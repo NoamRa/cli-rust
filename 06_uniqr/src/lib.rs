7c5eba2cb820277942da3d7dae80fcf606ce072a
NoamR <noam.raby@up42.com>
2023-10-11 10:56:30 +0200

    06 uniqr done

--- 06_uniqr/src/lib.rs
+++ 06_uniqr/src/lib.rs
@@ -1,5 +1,9 @@
 use clap::{App, Arg};
-use std::error::Error;
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader, Write},
+};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -39,12 +43,60 @@ pub fn get_args() -> MyResult<Config> {
 
     Ok(Config {
         in_file: matches.value_of("input_file").unwrap().to_string(),
-        out_file: Some(matches.value_of("output_file").unwrap().to_string()),
+        out_file: matches.value_of("output_file").map(String::from),
         count: matches.is_present("count"),
     })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
+    let mut in_file = open(&config.in_file).map_err(|e| format!("{}: {}", config.in_file, e))?;
+
+    let mut out_file: Box<dyn Write> = match &config.out_file {
+        Some(out_name) => Box::new(File::create(out_name)?),
+        _ => Box::new(io::stdout()),
+    };
+
+    let mut print = |count: u32, text: String| -> MyResult<()> {
+        if count > 0 {
+            if config.count {
+                write!(out_file, "{count:4} {text}")?;
+            } else {
+                write!(out_file, "{text}")?;
+            };
+        };
+        Ok(())
+    };
+
+    let mut line = String::new();
+    let mut previous_line = String::new();
+    let mut count: u32 = 0;
+
+    loop {
+        let bytes = in_file.read_line(&mut line)?;
+        // terminate loop on EOF
+        if bytes == 0 {
+            break;
+        }
+
+        // check line content excluding line endings
+        if line.trim_end() != previous_line.trim_end() {
+            print(count, previous_line)?;
+            previous_line = line.clone();
+            count = 0;
+        }
+        count += 1;
+        line.clear(); // otherwise read_line will append more bytes to line
+    }
+
+    // print last results
+    print(count, previous_line)?;
+
     Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
